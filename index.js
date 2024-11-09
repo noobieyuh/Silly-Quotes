19ccb90990b31660407a02fab9ce2b99ad15d401
noobie <150881131+noobieoncrack@users.noreply.github.com>
2024-11-09 15:32:36 -0600

    added more lines of code

--- index.js
+++ index.js
@@ -2,4 +2,34 @@ function quote()
 {
     var clickSound = new Audio("click.wav");
     clickSound.play();
+
+    var quote = "";
+
+    var quoteSelection = (Math.floor(Math.random() * 10) + 1);
+    console.log(quoteSelection);
+
+    if(quoteSelection == 1)
+        {
+            quote = '"Syntax errors, more like, stupid errors >:("';
+        }
+    if(quoteSelection == 2)
+        {
+            quote = '"Code is love, code is life <3"';
+        }
+    if(quoteSelection == 3)
+        {
+            quote = '"Time is moving as time goes on c:"';
+        }
+    if(quoteSelection == 4)
+        {
+            quote = '"Flowers are so pretty :D"';
+        }
+    if(quoteSelection == 5)
+        {
+            quote = '"If you are right, then you are not wrong :o"'
+        }
+
+
+
+    document.getElementById("insertQuote").innerHTML = quote;
 }
